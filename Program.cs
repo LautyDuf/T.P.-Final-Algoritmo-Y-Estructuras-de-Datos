534bc88e72c280b62d87b32831354fb432112ac8
LautyDuf <138404410+LautyDuf@users.noreply.github.com>
2026-05-25 14:51:50 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+class Program
+{
+public static void Main(string[] args)
+{
+Console.WriteLine("------------------ Trabajo Integrador ------------------");
+Console.WriteLine();
+
+// TODO: Implement Functionality Here
+
+Console.Write("Press any key to continue . . . ");
+Console.ReadKey(true);
+}
+}
+} 
+
