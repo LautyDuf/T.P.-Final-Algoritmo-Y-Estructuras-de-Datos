ba4f038051354dbbe145f2f18de504d5707ab0b7
LautyDuf <138404410+LautyDuf@users.noreply.github.com>
2026-06-03 19:29:44 -0300

    Update Heap.cs

--- Heap.cs
+++ Heap.cs
@@ -21,16 +21,13 @@ namespace tpfinal
 
         public Dato tope()
         {
-            if (esVacia())
-                return null;
-
+            if (esVacia()) return null;
             return elementos[0];
         }
 
         public bool agregar(Dato elem)
         {
-            if (elem == null)
-                return false;
+            if (elem == null) return false;
 
             elementos.Add(elem);
             heapifyUp(elementos.Count - 1);
@@ -39,17 +36,14 @@ namespace tpfinal
 
         public Dato eliminar()
         {
-            if (esVacia())
-                return null;
+            if (esVacia()) return null;
 
             Dato eliminado = elementos[0];
-
             int ultimo = elementos.Count - 1;
             elementos[0] = elementos[ultimo];
             elementos.RemoveAt(ultimo);
 
-            if (!esVacia())
-                heapifyDown(0);
+            if (!esVacia()) heapifyDown(0);
 
             return eliminado;
         }
@@ -59,28 +53,22 @@ namespace tpfinal
             while (i > 0)
             {
                 int padre = (i - 1) / 2;
-
                 bool condicion = false;
 
-                // Compara usando la variable 'ocurrencia' en minúscula
                 if (esMaxHeap)
                 {
-                    if (elementos[i].ocurrencia > elementos[padre].ocurrencia)
-                        condicion = true;
+                    if (elementos[i].ocurrencia > elementos[padre].ocurrencia) condicion = true;
                 }
                 else
                 {
-                    if (elementos[i].ocurrencia < elementos[padre].ocurrencia)
-                        condicion = true;
+                    if (elementos[i].ocurrencia < elementos[padre].ocurrencia) condicion = true;
                 }
 
-                if (!condicion)
-                    break;
+                if (!condicion) break;
 
                 Dato aux = elementos[i];
                 elementos[i] = elementos[padre];
                 elementos[padre] = aux;
-
                 i = padre;
             }
         }
@@ -88,7 +76,6 @@ namespace tpfinal
         private void heapifyDown(int i)
         {
             int n = elementos.Count;
-
             while (true)
             {
                 int izq = 2 * i + 1;
@@ -97,23 +84,19 @@ namespace tpfinal
 
                 if (izq < n)
                 {
-                    if (elementos[izq].ocurrencia > elementos[candidato].ocurrencia)
-                        candidato = izq;
+                    if (elementos[izq].ocurrencia > elementos[candidato].ocurrencia) candidato = izq;
                 }
 
                 if (der < n)
                 {
-                    if (elementos[der].ocurrencia > elementos[candidato].ocurrencia)
-                        candidato = der;
+                    if (elementos[der].ocurrencia > elementos[candidato].ocurrencia) candidato = der;
                 }
 
-                if (candidato == i)
-                    break;
+                if (candidato == i) break;
 
                 Dato aux = elementos[i];
                 elementos[i] = elementos[candidato];
                 elementos[candidato] = aux;
-
                 i = candidato;
             }
         }
